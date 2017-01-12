e6deeaa3730235ce7f2a3db9682ea40f9943d6f8
sarah leahy <sarah leahy>
2017-01-12 11:12:24 -0700

    add pics and show/hide pics

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,19 @@
 $(document).ready(function(){
+  $("#massage").click(function() {
+    $("#massage-pic").toggle();
+  });
   $("#appointment form").submit(function(event) {
     var nameInput = $("input#name").val();
     var dateInput = $("input#date").val();
-    var serviceInput = $("input#service").val();
+    var service = $("#service").val();
     var timeInput = $("input#time").val();
 
-  $(".name").text(nameInput);
-  $(".date").text(dateInput);
-  $(".time").text(timeInput);
-  $(".service").text(serviceInput);
+    $(".name").text(nameInput);
+    $(".date").text(dateInput);
+    $(".time").text(timeInput);
+    $(".service").text(service);
 
-  event.preventDefault();
+
+    event.preventDefault();
   });
 });
